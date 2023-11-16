753d05fad28a12ca86f53657e7cd40c2228aecd4
curtis-loupe <curtis@loupe.team>
2023-11-16 10:16:28 -0700

    Start fresh

--- example/HMI/src/app.js
+++
@@ -1,6 +0,0 @@
-machine = new WEBHMI.Machine({
-    port: 8000,
-    ipAddress: '127.0.0.1',
-    maxReconnectCount: 5000
-  });  
-  setInterval(WEBHMI.updateHMI,30)
--- node_modules/@loupeteam/development-template/example/AsProject/README.md
+++
@@ -1,6 +0,0 @@
-To start a as project use
-
-```
-lpm init
-lpm install starterasproject
-```
