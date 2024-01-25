41af74939e8fbdeee394306b9175d56f9c03f80d
curtis-loupe <curtis@loupe.team>
2024-01-25 15:37:31 -0700

    Check if user exists & export users on cmd

--- example/AsProject/Logical/LoginLvlServer/Cyclic.c
+++ example/AsProject/Logical/LoginLvlServer/Cyclic.c
@@ -58,29 +58,63 @@ void _CYCLIC ProgramCyclic(void)
 		// Parse the queryJSON to extract the username and password and set to PV's
 		task.internal.parsedQuery.status = JsmnParse((UDINT)&task.internal.parser, (UDINT)&task.internal.queryJSON, brsstrlen((UDINT)&task.internal.queryJSON), (UDINT)&task.internal.tokens, sizeof(task.internal.tokens)/sizeof(task.internal.tokens[0]));
 		
-		// ----------------- GET USER LEVEL -----------------	
-		//Get return from ArUserAuthenticatePassword
-		// if Authentic
-			// Set User Level - Using ArUserGetProperty
-		// if not 
-			// Set User Level = LOGGED_OUT;
+		task.internal.ArUser.HasRole_FB.Execute = 1;
 		
+		} // End if for new response on expected uri
+	
+	// ----------------- GET USER LEVEL -----------------	
+	//Get return from ArUserAuthenticatePassword
+	// if Authentic
+	// Set User Level - Using ArUserGetProperty
+	// if not 
+	// Set User Level = LOGGED_OUT;
+	
+	memcpy(&task.internal.ArUser.HasRole_FB.UserName, &task.internal.parsedQuery.data.userName, strlen(task.internal.parsedQuery.data.userName));
+	strcpy(&task.internal.ArUser.HasRole_FB.RoleName,"Administrators");
+	
+	ArUserHasRole(&task.internal.ArUser.HasRole_FB);
+
+	// Check that the FUB is complete
+	if (task.internal.ArUser.HasRole_FB.Done && !task.internal.ArUser.HasRole_FB.Busy) {
+		task.internal.ArUser.HasRole_FB.Execute = 0;
+	} else if (task.internal.ArUser.HasRole_FB.Error) {
+		// TODO: check against error ID to see if exists
+		task.status.error = 1;
+	}
+	
+	// ----------------- EXPORT USERS (FOR DEBUG) -----------------
+	if(task.cmd.exportUsers) {
 		
-		// ... Temp until ARUser is implemented
-		task.internal.userLvl = LOGGED_OUT;
+		// Export List of user to File for debugging
+		memcpy(&task.internal.ArUser.Export_FB.FilePath, &task.internal.ArUser.FilePath, strlen(task.internal.ArUser.FilePath));
+		task.internal.ArUser.Export_FB.Execute = 1;
+		// Run the Export FUB
+		ArUserExport(&task.internal.ArUser.Export_FB);
+		// Check that the FUB is complete
+		if (task.internal.ArUser.Export_FB.Done && !task.internal.ArUser.Export_FB.Busy) {
+			task.internal.ArUser.Export_FB.Execute = 0;
+			task.cmd.exportUsers = 0;
+		} else if (task.internal.ArUser.Export_FB.Error) {
+			task.status.error = 1;
+		}
+		// Reset the command
+		task.cmd.exportUsers = 0;
+	}	
 	
 
-		// ----------------- SEND RESPONSE WITH USER LEVEL -----------------	
-		// Create JSON String
-		ChopRender((UDINT)&task.internal.sendBuffer.message, (UDINT)&task.internal.sendBuffer.template, sizeof(task.internal.sendBuffer.message),(UDINT)&task.internal.sendBuffer.messageLength);
-		// Send the response
-		task.internal.response.send = task.internal.response.send && !task.internal.response.done; // Reset after message is sent
-		task.internal.response.pContent = task.internal.sendBuffer.message;
-		task.internal.response.contentLength = strlen(task.internal.sendBuffer.message);
-		task.internal.response.send = 1;
-		task.internal.response.status = LLHTTP_STAT_OK;
+
+	// TODO: This is not sending now that it is outside the newRequest if statement
+	// ----------------- SEND RESPONSE WITH USER LEVEL -----------------	
+	// Create JSON String
+	ChopRender((UDINT)&task.internal.sendBuffer.message, (UDINT)&task.internal.sendBuffer.template, sizeof(task.internal.sendBuffer.message),(UDINT)&task.internal.sendBuffer.messageLength);
+	// Send the response
+	task.internal.response.send = task.internal.response.send && !task.internal.response.done; // Reset after message is sent
+	task.internal.response.pContent = task.internal.sendBuffer.message;
+	task.internal.response.contentLength = strlen(task.internal.sendBuffer.message);
+	task.internal.response.send = 1;
+	task.internal.response.status = LLHTTP_STAT_OK;
 		
-	} // End if for new response on expected uri
+	
 
 } // End cyclic
 
