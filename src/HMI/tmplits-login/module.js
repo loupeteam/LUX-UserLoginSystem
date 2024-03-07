78e40339bc9fa262b94447d9e59542c2f4c691cc
curtis-loupe <curtis@loupe.team>
2024-03-07 11:59:31 -0700

    Rename the functions with a TmplitLogin prefix

--- src/HMI/tmplits-login/module.js
+++ src/HMI/tmplits-login/module.js
@@ -8,7 +8,7 @@
 import * as util from "../tmplits-utilities/module.js"
 
 
-export function DefaultLogin(url, username, password, onSuccess) { 
+export function TmplitLoginDefaultFetch(url, username, password, onSuccess) { 
 // this is the object thtat the method is owned by
 
 fetch(url+'?' + new URLSearchParams({userName: username, password: password}),{
@@ -32,7 +32,7 @@ fetch(url+'?' + new URLSearchParams({userName: username, password: password}),{
 }
 
 // Function will get executed on click of submit button 
-export function SubmitForm(e) {
+export function TmplitLoginSubmitForm(e) {
     e.preventDefault();
 
     // Find the scope for this form instance
@@ -54,13 +54,13 @@ export function SubmitForm(e) {
     let localLoginServerIp = scopeLogin.getAttribute('data-server-ip') // localLoginServerIp is the string assigned to the LoginServerIp js var via aliasing with the data-server-ip attribute
     
     // Call the default login function with a callback function   
-    DefaultLogin("http://"+ localLoginServerIp + ":1238/getLoginLvl", username, password,(level)=>{
+    TmplitLoginDefaultFetch("http://"+ localLoginServerIp + ":1238/getLoginLvl", username, password,(level)=>{
         // Set machine user level from response           
         localMachine.setUserLevel(level);
     }) // Define the callback using a Closure to allow for lexical scoping
 };
  
-export function OpenModal(e) {
+export function TmplitLoginOpenModal(e) {
     e.preventDefault();
     // Find the scope for this tmplit instance
     let scope = e.target.classList.contains('lui-login-scope') ? e.target : e.target.closest('.lui-login-scope');
@@ -70,7 +70,7 @@ export function OpenModal(e) {
     
 };
 
-export function CloseModal(e) {
+export function TmplitLoginCloseModal(e) {
     e.preventDefault();
     // Find the scope for this tmplit instance
     let scope = e.target.classList.contains('lui-login-scope') ? e.target : e.target.closest('.lui-login-scope');
@@ -79,7 +79,7 @@ export function CloseModal(e) {
     modal.classList.remove("show");
     
 };
-export function Logout(e) {
+export function TmplitLoginLogoutBtn(e) {
     e.preventDefault();
     // Find the scope for the overall tmplit instance
     let scopeLogin = e.target.classList.contains('lui-login-scope') ? e.target : e.target.closest('.lui-login-scope');
@@ -111,14 +111,14 @@ export function TmplitLogin(context, args) {
     // TODO: Add support if user has passed in a PV (immediatly setUserPV)
 
 
-    return ` 
+    return `
         <div class="lui-login-scope" data-machine-name=${dataMachineName} data-server-ip=${loginServerIp} >
         <!-- Modal Trigger-->
-        <button class="btn btn-primary lui-loginBtn" onclick="OpenModal(event)">
+        <button class="btn btn-primary lui-loginBtn" onclick="TmplitLoginOpenModal(event)">
             Login
         </button>
 
-        <button class="btn btn-primary lui-logoutBtn" min-user-level-unlock="1" onclick="Logout(event)">
+        <button class="btn btn-primary lui-logoutBtn" min-user-level-unlock="1" onclick="TmplitLoginLogoutBtn(event)">
             Log Out
         </button>
         
@@ -129,7 +129,7 @@ export function TmplitLogin(context, args) {
                     
                     <!-- Modal Header -->
                     <div class="modal-header">
-                        <button type="button" class="close" onclick="CloseModal(event)">
+                        <button type="button" class="close" onclick="TmplitLoginCloseModal(event)">
                             <span>&times;</span>
                             <span class="sr-only">Close</span>
                         </button>
@@ -150,14 +150,14 @@ export function TmplitLogin(context, args) {
                             <label for="loginPass">Password</label>
                             <input type="password" class="form-control lui-loginPass" placeholder="Password"/>
                         </div>
-                        <button type="submit" class="btn btn-default" onclick="SubmitForm(event)">Submit</button>
+                        <button type="submit" class="btn btn-default" onclick="TmplitLoginSubmitForm(event)">Submit</button>
                         </form>
                         
                     </div>
                     
                     <!-- Modal Footer -->
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" onclick="CloseModal(event)"> Close </button>
+                        <button type="button" class="btn btn-default" onclick="TmplitLoginCloseModal(event)"> Close </button>
                     </div>
                 </div>
             </div>
